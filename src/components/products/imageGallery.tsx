5515e335e7c629f2a6000aa87a03091e8c30e665
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2025-12-29 17:13:16 +0100

    image gallery done

--- src/components/products/imageGallery.tsx
+++ src/components/products/imageGallery.tsx
@@ -5,7 +5,7 @@ const ImageGallery = () => {
     <section className=' bg-white'>
 
 {/* desktop  */}
-    <div className="py-16 md:flex-row justify-center items-start gap-3 md:px-12 lg:px-20 hidden md:flex">
+    <div className="py-16 md:flex-row justify-center items-start gap-3 w-[90%] hidden md:flex">
     <div className='flex flex-col gap-2 justify-between'>
       <Image src={'/images/pngs/gallery1.png'} alt='goats' className='fill' width={213} height={434}/>
       <Image src={'/images/pngs/gallery2.png'} alt='rams' width={213} height={212}/>
@@ -36,30 +36,30 @@ const ImageGallery = () => {
     </div>
 
     {/* mobile  */}
-    <div className='flex flex-col gap-3 md:hidden py-12 w-[80%] mx-auto'>
+    <div className='flex flex-col gap-3 md:hidden py-12 w-[85%] mx-auto'>
       <div className='flex gap-1 justify-between'>
 <div className='flex flex-col gap-2'>
-      <Image src={'/images/pngs/gallery1.png'} alt='goats' className='fill' width={153} height={311}/>
-      <Image src={'/images/pngs/gallery2.png'} alt='rams' width={153} height={151}/>
+      <Image src={'/images/pngs/gallery1.png'} alt='goats' className='fill' width={183} height={311}/>
+      <Image src={'/images/pngs/gallery2.png'} alt='rams' width={183} height={151}/>
     </div>
     <div className='flex flex-col gap-1 justify-between'>
-      <Image src={'/images/pngs/gallery3.png'} alt='sheep' width={187} height={152}/>
-      <Image src={'/images/pngs/gallery4.png'} alt='men' width={187} height={311}/>
+      <Image src={'/images/pngs/gallery3.png'} alt='sheep' width={220} height={152}/>
+      <Image src={'/images/pngs/gallery4.png'} alt='men' width={220} height={321} className='h-full'/>
     </div>
       </div>
       <div className='flex gap-2 justify-between'>
         <div className='flex flex-col gap-2 justify-between'>
-          <Image src={'/images/pngs/gallery5.png'} alt='men' width={191} height={148}/>
-          <Image src={'/images/pngs/gallery7.png'} alt='men' width={191} height={189}/>
+          <Image src={'/images/pngs/gallery5.png'} alt='men' width={220} height={148}/>
+          <Image src={'/images/pngs/gallery7.png'} alt='men' width={220} height={199} className='w-full h-full'/>
         </div>
         <div className='flex flex-col gap-2 justify-between'>
-          <Image src={'/images/pngs/gallery11Mobile.png'} alt='leaves' width={151} height={200} />
-          <Image src={'/images/pngs/gallery6.png'} alt='dog' width={151} height={106}/>
+          <Image src={'/images/pngs/gallery11Mobile.png'} alt='leaves' width={181} height={200} />
+          <Image src={'/images/pngs/gallery6.png'} alt='dog' width={181} height={106}/>
         </div>
       </div>
       <div className='flex flex-col gap-2 justify-between'>
-        <Image src={'/images/pngs/gallery10Mobile.png'} alt='rams' width={350} height={121}/>
-        <Image src={'/images/pngs/gallery8.png'} alt='sheep' width={350} height={254}/>
+        <Image src={'/images/pngs/gallery10Mobile.png'} alt='rams' width={350} height={121} className='w-full'/>
+        <Image src={'/images/pngs/gallery8.png'} alt='sheep' width={350} height={254} className='w-full'/>
       </div>
     </div>
     </section>
